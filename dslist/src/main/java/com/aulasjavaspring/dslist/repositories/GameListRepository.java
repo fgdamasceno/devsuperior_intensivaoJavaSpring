ffae200235363c7219303f8db3b251ce92c1da58
fgdamasceno <fgdamasceno@gmail.com>
2024-11-14 16:11:49 -0300

    move games on list

--- dslist/src/main/java/com/aulasjavaspring/dslist/repositories/GameListRepository.java
+++ dslist/src/main/java/com/aulasjavaspring/dslist/repositories/GameListRepository.java
@@ -1,11 +1,16 @@
 package com.aulasjavaspring.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.aulasjavaspring.dslist.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
-	
+
 	// REALIZA AS CONSULTAS/OPERAÇÕES NO BANCO DE DADOS
-	
+	@Modifying
+	@Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
+
 }
